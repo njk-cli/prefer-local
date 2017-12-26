7bad2cf591de3f56241b448f25590119fd83dcc0
Mohit Singh <mohitsinghs.dev@gmail.com>
2017-12-26 06:40:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const has = require('lodash.has')
+
+/**
+ * If an Object have an inner property with a boolean value
+ * and have a global value then returns a boolean value
+ * preferring inner property over global.
+ *
+ * @param {Object}  src  - source object containing inner property
+ * @param {string}  key  - key of the inner property
+ * @param {boolean} prop - value of global property
+ *
+ * @returns {boolean}
+ */
+module.exports = (src, key, prop) => {
+  let propExist = has(src, key)
+  return (propExist && src[key]) || (!propExist && prop)
+}
